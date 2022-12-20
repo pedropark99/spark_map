fd50187f29581b467ac297bfa96e9141fd2ace6b
pedropark99 <pedropark99@gmail.com>
2022-12-20 19:35:03 -0300

    Fix type of columns in transf parquet file

--- tests/functions/test_functions.py
+++ tests/functions/test_functions.py
@@ -1,2 +1,8 @@
-from spark_map.functions import spark_map
+#from spark_map.functions import spark_map
+from pyspark.sql import SparkSession
+spark = SparkSession.builder.getOrCreate()
 
+
+students = spark.read.parquet("data/students.parquet")
+transf = spark.read.parquet("data/transf.parquet")
+transf.printSchema()
