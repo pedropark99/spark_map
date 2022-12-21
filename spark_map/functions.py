30ae6ac0012c2a52e96860318678a35d17701f4d
pedropark99 <pedropark99@gmail.com>
2022-12-20 21:01:32 -0300

    Aesthetic changes to indentation of 4 spaces in all source code

--- spark_map/functions.py
+++ spark_map/functions.py
@@ -1,79 +1,84 @@
 from pyspark.sql.functions import column
 from pyspark.sql import DataFrame, GroupedData
-
+from typing import Callable
 from spark_map.mapping import __map_columns
 
 
 
-def spark_map(table, mapping, function):
-  """
-  With `spark_map()`, you can apply a aggregate function to a set of columns in a spark DataFrame.
-  It receives a spark DataFrame as input, and returns a new aggregated spark DataFrame as output.
-  For example, to apply the `mean()` function, to the third, fourth and fifth columns, you do: 
-  
-  Example
-  --------
-  >>> import pyspark.sql.functions as F
-  >>> tb = spark.table('sales.sales_by_day')
-  >>> spark_map(tb, at_position(3,4,5), F.mean)
-  """
-  if __is_spark_grouped_data(table):
-    cols = table._df.columns
-    schema = list(table._df.schema)
-  if __is_spark_dataframe(table):
+def spark_map(table: DataFrame, mapping: dict, function: Callable):
+    """
+    With `spark_map()`, you can apply a aggregate function to a set of columns in a spark DataFrame.
+    It receives a spark DataFrame as input, and returns a new aggregated spark DataFrame as output.
+    For example, to apply the `mean()` function, to the third, fourth and fifth columns, you do: 
+    
+    Example
+    --------
+    >>> import pyspark.sql.functions as F
+    >>> tb = spark.table('sales.sales_by_day')
+    >>> spark_map(tb, at_position(3,4,5), F.mean)
+    """
+    if __is_spark_grouped_data(table):
+        cols = table._df.columns
+        schema = list(table._df.schema)
+    if __is_spark_dataframe(table):
+        cols = table.columns
+        schema = list(table.schema)
+    
+    mapping = __map_columns(mapping, cols, schema)
+    __report_mapped_columns(mapping)
+    
+    params = []
+    for col in mapping:
+        params.append(
+            function(column(col)).alias(col)
+        )
+      
+    result = table.agg(*params)
+    
+    return result
+
+
+
+def spark_across(table: DataFrame, mapping: dict, function: Callable, **kwargs):
+    """
+    With `spark_across()` you can apply a function across multiple columns of a spark DataFrame.
+    While `spark_map()` calculates aggregates in a set of columns, `spark_across()` uses
+    `withColumn()` to apply a function over each row of a set of columns.
+
+    Example
+    --------
+    >>> import pyspark.sql.functions as F
+    >>> tb = spark.table('sales.sales_by_day')
+    >>> spark_across(tb, at_position(3,4,5), F.cast, 'double')
+    """
+    if __is_spark_grouped_data(table):
+        raise ValueError("You gave a grouped Spark DataFrame to `spark_across()`. However, this function work solely with plain Spark DataFrames. Did you meant to use `spark_map()` instead?")
+    
     cols = table.columns
     schema = list(table.schema)
-  
-  mapping = __map_columns(mapping, cols, schema)
-  __report_mapped_columns(mapping)
-  
-  params = []
-  for col in mapping:
-    params.append(function(column(col)).alias(col))
     
-  result = table.agg(*params)
-  
-  return result
+    mapping = __map_columns(mapping, cols, schema)
+    __report_mapped_columns(mapping)
+    
+    for col in mapping:
+        table = table.withColumn(
+            col,
+            function(column(col), **kwargs)
+        )
 
-
-
-def spark_across(table:DataFrame, mapping, function, **kwargs):
-  """
-  With `spark_across()` you can apply a function across multiple columns of a spark DataFrame.
-  While `spark_map()` calculates aggregates in a set of columns, `spark_across()` uses
-  `withColumn()` to apply a function over each row of a set of columns.
-
-  Example
-  --------
-  >>> import pyspark.sql.functions as F
-  >>> tb = spark.table('sales.sales_by_day')
-  >>> spark_across(tb, at_position(3,4,5), F.cast, 'double')
-  """
-  if __is_spark_grouped_data(table):
-    raise ValueError("You gave a grouped Spark DataFrame to `spark_across()`. However, this function work solely with plain Spark DataFrames. Did you meant to use `spark_map()` instead?")
-  
-  cols = table.columns
-  schema = list(table.schema)
-  
-  mapping = __map_columns(mapping, cols, schema)
-  __report_mapped_columns(mapping)
-  
-  for col in mapping:
-    table = table.withColumn(col, function(col, **kwargs))
-  
-  return table
+    return table
 
 
 
 
 
 def __is_spark_dataframe(x):
-  return isinstance(x, DataFrame)
+    return isinstance(x, DataFrame)
 
 def __is_spark_grouped_data(x):
-  return isinstance(x, GroupedData)
+    return isinstance(x, GroupedData)
 
 
-def __report_mapped_columns(mapping:list[str]):
-  message = f"Selected columns by `spark_map()`: {', '.join(mapping)}\n"
-  print(message)
+def __report_mapped_columns(mapping: list[str]):
+    message = f"Selected columns by `spark_map()`: {', '.join(mapping)}\n"
+    print(message)
--- spark_map/mapping.py
+++ spark_map/mapping.py
@@ -1,91 +1,91 @@
 import re
 from pyspark.sql.types import StructType
 from pyspark.sql.types import (
-  StringType
-  , IntegerType
-  , LongType
-  , DoubleType
-  , DateType
-  , TimestampType
+    StringType
+    , IntegerType
+    , LongType
+    , DoubleType
+    , DateType
+    , TimestampType
 )
 
 
 
 from spark_map.utils import (
-  __check_list_input
-  , __check_string_input
-  , __is_string
+    __check_list_input
+    , __check_string_input
+    , __is_string
 )
 
 
 
 
     
-def all_of(list_cols:list[str]):
-  __check_list_input(list_cols, str, "all_of()")
-  return {'fun': 'all_of', 'val': list_cols}
+def all_of(list_cols: list[str]):
+    __check_list_input(list_cols, str, "all_of()")
+    return {'fun': 'all_of', 'val': list_cols}
     
-def matches(regex:str):
-  __check_string_input(regex, "matches()")
-  return {'fun': "matches", 'val': regex} 
+def matches(regex: str):
+    __check_string_input(regex, "matches()")
+    return {'fun': "matches", 'val': regex} 
 
   
-def at_position(*indexes:list[int], zero_index:bool = False):
-  __check_list_input(indexes, int, "at_position()")
+def at_position(*indexes: list[int], zero_index: bool = False):
+    __check_list_input(indexes, int, "at_position()")
 
-  if zero_index == False:
-    indexes = [index - 1 for index in indexes]
-  # Check if any of the indexes are negative:
-  negative = [index < 0 for index in indexes]
-  if any(negative):
-    raise ValueError("One (or more) of the provided indexes are negative! Did you provided a zero index, and not set the `zero_index` argument to True?")
+    if zero_index == False:
+        indexes = [index - 1 for index in indexes]
+    # Check if any of the indexes are negative:
+    negative = [index < 0 for index in indexes]
+    if any(negative):
+        raise ValueError("One (or more) of the provided indexes are negative! Did you provided a zero index, and not set the `zero_index` argument to True?")
 
-  # Transform to `set` to avoid duplicated indexes
-  indexes = list(set(indexes))
-  indexes.sort()
-  return {'fun': "at_position", 'val': indexes}
-
-
-def starts_with(text:str):
-  __check_string_input(text, "starts_with()")
-  return {'fun': "starts_with", 'val': text}
-
-def ends_with(text:str):
-  __check_string_input(text, "ends_with()")
-  return {'fun': "ends_with", 'val': text}
-
-def are_of_type(arg_type:str):
-  __check_string_input(arg_type, "are_of_type()")
-  valid_types = ['string', 'int', 'long', 'double', 'date', 'datetime']
-  if arg_type not in valid_types:
-    types = [f"'{t}'" for t in valid_types]
-    types = ', '.join(types)
-    message = f'You must choose one of the following values: {types}'
-    raise ValueError(message)
-  return {'fun': "are_of_type", 'val': arg_type}
+    # Transform to `set` to avoid duplicated indexes
+    indexes = list(set(indexes))
+    indexes.sort()
+    return {'fun': "at_position", 'val': indexes}
 
 
+def starts_with(text: str):
+    __check_string_input(text, "starts_with()")
+    return {'fun': "starts_with", 'val': text}
+
+def ends_with(text: str):
+    __check_string_input(text, "ends_with()")
+    return {'fun': "ends_with", 'val': text}
+
+def are_of_type(arg_type: str):
+    __check_string_input(arg_type, "are_of_type()")
+    valid_types = ['string', 'int', 'long', 'double', 'date', 'datetime']
+    if arg_type not in valid_types:
+        types = [f"'{t}'" for t in valid_types]
+        types = ', '.join(types)
+        message = f'You must choose one of the following values: {types}'
+        raise ValueError(message)
+    return {'fun': "are_of_type", 'val': arg_type}
 
 
 
 
 
-def __map_columns(mapping, cols:list, schema:StructType):
-  mapping_function = mapping['fun']
-  mapping_value = mapping['val']
-  if __is_string(mapping_function):
-    m = Mapping()
-    method_to_call = getattr(m, mapping_function)
-    method_to_call(mapping_value, cols, schema)
-    selected_cols = m.mapped_cols
-  else:
-    selected_cols = mapping_function(mapping_value, cols, schema)
-  
-  if len(selected_cols) == 0:
-    message = "`spark_map()` did not found any column that matches your mapping!"
-    raise KeyError(message)
-  
-  return selected_cols
+
+
+def __map_columns(mapping: dict, cols: list[str], schema: StructType):
+    mapping_function = mapping['fun']
+    mapping_value = mapping['val']
+    if __is_string(mapping_function):
+        m = Mapping()
+        method_to_call = getattr(m, mapping_function)
+        method_to_call(mapping_value, cols, schema)
+        selected_cols = m.mapped_cols
+    else:
+        selected_cols = mapping_function(mapping_value, cols, schema)
+    
+    if len(selected_cols) == 0:
+        message = "`spark_map()` did not found any column that matches your mapping!"
+        raise KeyError(message)
+    
+    return selected_cols
 
 
 
@@ -98,53 +98,53 @@ def __map_columns(mapping, cols:list, schema:StructType):
 class Mapping:
     
     def __init__(self):
-      self.mapped_cols = []
+        self.mapped_cols = []
     
     
   
-    def all_of(self, list_cols:list[str], cols:list[str], schema:StructType):
-      selected_cols = [col for col in list_cols if col in cols]
-      self.mapped_cols = selected_cols
+    def all_of(self, list_cols: list[str], cols: list[str], schema: StructType):
+        selected_cols = [col for col in list_cols if col in cols]
+        self.mapped_cols = selected_cols
 
-    def at_position(self, indexes:list[int], cols:list[str], schema:StructType):
-      selected_cols = [cols[i] for i in indexes]
-      self.mapped_cols = selected_cols
+    def at_position(self, indexes: list[int], cols: list[str], schema: StructType):
+        selected_cols = [cols[i] for i in indexes]
+        self.mapped_cols = selected_cols
 
-    def ends_with(self, text:str, cols:list[str], schema:StructType):
-      selected_cols = list()
-      for col in cols:
-        if col.endswith(text):
-            selected_cols.append(col)
+    def ends_with(self, text: str, cols: list[str], schema: StructType):
+        selected_cols = list()
+        for col in cols:
+            if col.endswith(text):
+                selected_cols.append(col)
 
-      self.mapped_cols = selected_cols
+        self.mapped_cols = selected_cols
 
-    def starts_with(self, text:str, cols:list[str], schema:StructType):
-      selected_cols = list()
-      for col in cols:
-        if col.startswith(text):
-            selected_cols.append(col)
+    def starts_with(self, text: str, cols: list[str], schema: StructType):
+        selected_cols = list()
+        for col in cols:
+            if col.startswith(text):
+                selected_cols.append(col)
 
-      self.mapped_cols = selected_cols
-
-
-    def matches(self, regex:str, cols:list[str], schema:StructType):
-      regex = re.compile(regex)
-      selected_cols = [col for col in cols if re.match(regex, col)]
-      self.mapped_cols = selected_cols
+        self.mapped_cols = selected_cols
 
 
-    def are_of_type(self, str_type:str, cols:list[str], schema:StructType):
-      valid_types = {
-        'int' : IntegerType(), 'double' : DoubleType(), 
-        'string' : StringType(), 'date' : DateType(),
-        'datetime' : TimestampType(), 'long': LongType()
-      }
+    def matches(self, regex: str, cols: list[str], schema: StructType):
+        regex = re.compile(regex)
+        selected_cols = [col for col in cols if re.match(regex, col)]
+        self.mapped_cols = selected_cols
 
-      target_type = valid_types[str_type]
-      selected_cols = list()
-      for name, field in zip(cols, schema):
-        if field.dataType == target_type:
-          selected_cols.append(name)
 
-      self.mapped_cols = selected_cols
+    def are_of_type(self, str_type: str, cols: list[str], schema: StructType):
+        valid_types = {
+            'int' : IntegerType(), 'double' : DoubleType(), 
+            'string' : StringType(), 'date' : DateType(),
+            'datetime' : TimestampType(), 'long': LongType()
+        }
+
+        target_type = valid_types[str_type]
+        selected_cols = list()
+        for name, field in zip(cols, schema):
+            if field.dataType == target_type:
+                selected_cols.append(name)
+
+        self.mapped_cols = selected_cols
 
