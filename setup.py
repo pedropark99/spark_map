6f51c782d3595288d5780f8d5f6293078f34dc97
pedropark99 <pedropark99@gmail.com>
2022-12-21 20:22:58 -0300

    Update version number, and fix formating of README in PyPI

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
-import os
-os.system("pandoc --from=markdown --to=rst --output=README.rst README.md")
-
+from pypandoc import convert_file
 from setuptools import setup
 import toml
+
+
+def read_file(path):
+    with open(path) as f:
+        lines = f.readlines()
+    text = ''.join(lines)
+    return text
+
 # Import package metadata from the pyproject.toml file.
 # This way we mantain these metadata in only one file, and just reuse as needed.
 metadata = toml.load("pyproject.toml")
 project = metadata['project']
-
 
 setup(
     name = project['name'],
@@ -16,7 +21,8 @@ setup(
     author_email = project['authors'][0]['email'],
     license = 'MIT License',
     description = project['description'],
-    long_description = "README.rst",
+    long_description = read_file(project['readme']),
+    long_description_content_type = "text/markdown",
     classifiers = project['classifiers'],
     url = project['urls']['Homepage'],
     keywords = ['spark', 'pyspark', 'map'],
